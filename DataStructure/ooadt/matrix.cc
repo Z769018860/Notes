4f554dade4dda7cd051270e249f67016e190d25f
AugustusWillisWang <zeweiwang@outlook.com>
2018-04-23 18:05:43 +0800

    Update notes.

--- DataStructure/ooadt/matrix.cc
+++ DataStructure/ooadt/matrix.cc
@@ -51,7 +51,10 @@ int Matrix::print() {}
 int Matrix::updatecnt() {}
 Matrix Matrix::transpose() {}
 
-class Matrix_Sparse : public Matrix
+//Sparse Matrix
+//----------------------------------------------------
+
+class Matrix_Sparse : public Matrix //todo : fix a->col b->row
 {
 public:
   Matrix_Sparse();
@@ -62,21 +65,26 @@ public:
   int print();
   virtual int updatecnt();
   Matrix_Sparse transpose_sparse();
-  friend Matrix_Sparse operator+(Matrix_Sparse tgt);
-  friend Matrix_Sparse operator-(Matrix_Sparse tgt);
-  friend Matrix_Sparse operator*(Matrix_Sparse tgt);
-  struct MSdot
+  friend Matrix_Sparse operator+(Matrix_Sparse tgt, Matrix_Sparse tgt2);
+  friend Matrix_Sparse operator-(Matrix_Sparse tgt, Matrix_Sparse tgt2);
+  friend Matrix_Sparse operator*(Matrix_Sparse tgt, Matrix_Sparse tgt2);
+  struct MSdot //todo : fix a->col b->row
   {
     int a;
     int b;
     int data;
+    struct MSdot operator=(int i)
+    {
+      this->data = i;
+    }
   };
 
   int addline(std::vector<int> src); //addline
-  int Matrix_Sparse::findpoint(int a, int b); //get the value of a point;
-  int Matrix_Sparse::setup_row_position();//reset row_position_
-  int Matrix_Sparse::set(int a, int b, int data);//get a point to given value
-  int Matrix_Sparse::sort();                     //sort the elements by row, update row_position_ and merge the same term at the same time;
+  int findpoint(int a, int b);       //get the value of a point;
+  int setup_row_position();          //reset row_position_
+  int set(int a, int b, int data);   //get a point to given value
+  int sort();                        //sort the elements by row, update row_position_ , will not merge the same term at the same time;
+  int merge();                       //merge the same term; should be called after every add-in operation.
 
 private:
   std::vector<MSdot> data_;
@@ -229,11 +237,243 @@ int Matrix_Sparse::sort()
 {
   row_cnt_.clear();
   row_position_.clear();
-  std::vector<int> temp(row_,0);
+  std::vector<int> temp(row_, 0);
   row_cnt_ = temp;
   //traverse and get row_cnt_
   //use temp to save how many point has been moved
+  int s = data_.size();
+  for (int i = 0; i < s; i++)
+  {
+    ++row_cnt_[data_[i].a];
+  }
+  int pc = 0;
+  for (int i = 0; i < row_; i++)
+  {
+    row_position_[i] = pc;
+    pc += row_cnt_[i];
+  }
+
+  std::vector<int> datatemp(s, 0);
+  int ta, tb;
+
+  for (int i = 0; i < s; i++)
+  {
+    ta = data_[i].a;
+    datatemp[row_position_[ta] + temp[ta]] = data_[i].data;
+    temp[ta] += 1;
+  }
+
+  data_.assign(datatemp.begin(), datatemp.end());
 }
+
+int Matrix_Sparse::merge()
+{
+  int l = data_.size();
+  int todelete[l];
+  for (int i = 0; i < l; i++)
+  {
+    todelete[l] = 0;
+  }
+  for (int a = 0; a < l; a++)
+  {
+    if (!todelete[l])
+    {
+      for (int b = a + 1; b < l; b++)
+      {
+        if (!todelete[l])
+        {
+          if (data_[a].a == data_[b].a && data_[a].b == data_[b].b)
+          {
+            data_[a].data += data_[b].data;
+            todelete[b] = 1;
+          }
+        }
+      }
+    }
+  }
+  for (int i = l; i >= 0; i--)
+  {
+    if (todelete[l])
+    {
+      data_.erase(data_.begin() + l);
+    }
+  }
+
+  elem_ = data_.size();
+}
+
+Matrix_Sparse::Matrix_Sparse(const Matrix_Sparse &src)
+{
+  this->data_.assign(src.data_.begin(), src.data_.end());
+  this->row_position_.assign(src.row_position_.begin(), src.row_position_.end());
+  this->row_cnt_.assign(src.row_cnt_.begin(), src.row_cnt_.end());
+}
+
+Matrix_Sparse::~Matrix_Sparse()
+{
+}
+
+Matrix_Sparse operator+(Matrix_Sparse tgt, Matrix_Sparse tgt2)
+{
+  Matrix_Sparse res = tgt;
+
+  if (tgt2.row_ > tgt.row_)
+    tgt.row_ = tgt2.row_;
+  if (tgt2.col_ > tgt.col_)
+    tgt.col_ = tgt2.col_;
+
+  int l = tgt2.data_.size();
+  for (int i = 0; i < l; i++)
+  {
+    res.data_.push_back(tgt2.data_.back());
+  }
+
+  res.merge();
+  res.sort();
+  res.elem_ = res.data_.size();
+  return res;
+}
+
+Matrix_Sparse operator-(Matrix_Sparse tgt, Matrix_Sparse tgt2) {}
+Matrix_Sparse operator*(Matrix_Sparse tgt, Matrix_Sparse tgt2) {}
+
+// Orthogonal Matrix
+//----------------------------------------------------
+
+class Matrix_Orthogonal : public Matrix
+{
+public:
+  Matrix_Orthogonal();
+  Matrix_Orthogonal(const Matrix_Orthogonal &src);
+  Matrix_Orthogonal::Matrix_Orthogonal(int col, int row);
+  Matrix_Orthogonal(const int *src, int row, int col);
+  Matrix_Orthogonal(std::vector<int> src);
+  ~Matrix_Orthogonal();
+  int print();
+  // virtual int updatecnt();
+  Matrix_Orthogonal transpose_orthogonal();
+  friend Matrix_Orthogonal operator+(Matrix_Orthogonal tgt, Matrix_Orthogonal tgt2);
+  friend Matrix_Orthogonal operator-(Matrix_Orthogonal tgt, Matrix_Orthogonal tgt2);
+  friend Matrix_Orthogonal operator*(Matrix_Orthogonal tgt, Matrix_Orthogonal tgt2);
+
+  typedef struct MONode
+  {
+    int col;
+    int row;
+    int data;
+    struct MONode *colnext;
+    struct MONode *rownext;
+  } * pMON;
+
+  // int Matrix_Orthogonal::addline(std::vector<int> src); //addline
+  int findpoint(int col, int row);     //get the value of a point;
+  int set(int col, int row, int data); //get a point to given value
+  int resize(int col, int row);        //resize the shape of the matrix
+
+private:
+  // std::vector<MSdot> data_;
+  // std::vector<int> row_position_;
+  // std::vector<int> row_cnt_;
+  std::vector<pMON> rowhead_;
+  std::vector<pMON> colhead_;
+  int row_ = 0;
+  int col_ = 0;
+};
+
+Matrix_Orthogonal::Matrix_Orthogonal()
+{
+  row_ = 0;
+  col_ = 0;
+}
+
+Matrix_Orthogonal::Matrix_Orthogonal(int col, int row)
+{
+  col_ = col;
+  row_ = row;
+  std::vector<pMON> temp1(col, nullptr);
+  std::vector<pMON> temp2(row, nullptr);
+  colhead_.assign(temp1.begin(), temp1.end());
+  rowhead_.assign(temp2.begin(), temp2.end());
+}
+
+Matrix_Orthogonal::Matrix_Orthogonal(const Matrix_Orthogonal &src)
+{
+  // copy according to row
+  this->row_ = rowhead_.size();
+  this->rowhead_.assign(src.rowhead_.begin(), src.rowhead_.end());
+  std::vector<pMON> temp(src.colhead_);
+  this->colhead_.assign(temp.begin(), temp.end());
+  for (int a = 0; a < row_; a++)
+  {
+    if (rowhead_[a])
+    {
+      // copynode
+      // update colhead_
+    }
+  }
+}
+
+Matrix_Orthogonal::Matrix_Orthogonal(const int *src, int row, int col)
+{
+  // todo
+}
+
+// Matrix_Orthogonal::Matrix_Orthogonal(std::vector<int> src);
+
+Matrix_Orthogonal::~Matrix_Orthogonal() //will free memory
+{
+  row_ = rowhead_.size();
+  for (int i = 0; i < row_; i++)
+  {
+    while (rowhead_[i])
+    {
+      auto ptr = rowhead_[i];
+      rowhead_[i] = rowhead_[i]->rownext;
+      delete ptr;
+    }
+  }
+}
+
+int Matrix_Orthogonal::print()
+{
+  for (int a = 0; a < row_; a++)
+  {
+    for (int b = 0; b < col_; b++)
+    {
+      std::cout << findpoint(b, a) << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+// virtual int updatecnt();
+Matrix_Orthogonal Matrix_Orthogonal::transpose_orthogonal() {}
+Matrix_Orthogonal operator+(Matrix_Orthogonal tgt, Matrix_Orthogonal tgt2);
+Matrix_Orthogonal operator-(Matrix_Orthogonal tgt, Matrix_Orthogonal tgt2);
+Matrix_Orthogonal operator*(Matrix_Orthogonal tgt, Matrix_Orthogonal tgt2);
+int Matrix_Orthogonal::findpoint(int col, int row) {} //get the value of a point;
+
+int Matrix_Orthogonal::set(int col, int row, int data) //get a point to given value
+{
+  if (row_ <= row || col_ <= col)
+    throw("Insert out of range.");
+  pMON pnewnode = new struct MONode;
+  pnewnode->col = col;
+  pnewnode->row = row;
+
+  pMON temp = colhead_[col];
+  if (!colhead_[col])
+  {
+    colhead_[col] = pnewnode;
+    pnewnode->colnext = 0;
+  }
+  else
+    while (temp && temp->row < row)
+    {
+      temp = temp->colnext;
+    }
+}
+
+int Matrix_Orthogonal::resize(int a, int b) {} //resize the shape of the matrix
 
 int main()
 {
--- UniversityEnglish/esport.md
+++ UniversityEnglish/esport.md
@@ -1,7 +1,5 @@
 # Esports express
 
---
-
 ## E-Sports and its industrial
 by: UCAS post
 
@@ -15,6 +13,141 @@ by: UCAS post
 * amator matches
 * exclusive interviews
 
+* where there is still value in the growing eSports space
 ---
 
-## Article 1 
+```mermaid
+graph LR;
+
+subgraph E-eSports
+
+subgraph pending_problems
+
+gender_difference
+cheating
+gambing
+
+end
+
+subgraph propersity
+
+capital
+ecosystem
+
+end
+
+subgraph society_influence
+
+subculture_of_esports
+
+subgraph regulation
+intel_property
+tax
+parential_notification
+end
+
+end
+
+end
+
+```
+
+---
+
+## Passage 1. A brief history of esports.
+
+William Wang, **zeweiwang@outlook.com** 
+
+
+---
+
+## Passage 2. Esports industrial: from the league to eSports participants.
+
+William Wang, **zeweiwang@outlook.com** 
+
+<!-- ref: https://www.forbes.com/sites/darrenheitner/2015/10/15/the-business-of-esports-is-on-pace-to-explode/#23ff0307438a
+
+One year ago, the word 'eSport' had no real significance. A culture of hardcore video game players existed, but they were just that -- people with a serious interest in playing video games for pleasure. Seemingly out of nowhere, the eSports industry started to be taken seriously . Mega-agency WME | IMG is representing professional eSports players and teams. Marketing and media agency rEvolution has made acquisitions in the space and created a new eSports consulting subsidiary rEvXP. Now, award-winning global engagement marketing agency GMR is getting involved in the eSports space with the announcement that it has launched its own eSports consulting practice.
+
+"You look at 35 million eSports fans in the U.S. and a 16-34 highly coveted demographic. Higher viewership for a global eSports tournament than sports that are so sacred in the U.S. is very eye opening," said Hill.
+
+A SuperData brief from May 2015 indicates that the global eSports market is worth $612 million and its audience is 134 million and growing. Per Eventbrite, 67% of eSports players crave an increase in events with 40% indicating an interest in live events outside of major cities and 38% demonstrating a willingness to travel to another country or continent for an eSports tournament.
+
+Canada-based Cineplex entered into a deal in September that causes it to pay $10 million to acquire WorldGaming's operational assets. WorldGaming is an eSports platform that promotes tournaments, leagues and gaming ladders. Cineplex plans on integrating eSports tournaments into its existing movie theaters.
+
+ref: https://www.forbes.com/sites/darrenheitner/2015/08/23/billions-of-dollars-will-soon-be-wagered-on-esports-competitions/#4d6ec4a4181b
+
+participants and spectators
+
+ref: http://www.businessinsider.com/invest-esports-stocks-companies-business-opportunities-2017-12
+
+Investment in the industry is largely driven by partnerships with other sports properties and leagues. Teams like the Miami Heat, Manchester City, West Ham, and the Philadelphia 76ers are investing in players and teams in the eSports space. It gives opportunities for more growth and fan base development while also creating new and appealing assets to sell to current and future corporate partners.
+
+Twitch and YouTube only add value to the industry as more stakeholders and events emerge, thus making broadcasting and streaming tournaments and competitions all the more in demand in this growing industry.
+
+ref: https://www.engadget.com/2015/07/31/the-business-of-esports-in-numbers/ -->
+
+<!-- ref: https://newzoo.com/insights/articles/industry-first-report-evolution-esports-ecosystem/
+
+ref: https://www.natlawreview.com/article/esports-insights-and-trends-staying-current-emerging-developments-booming-esports-0 -->
+
+544
+### 1. Introduction
+
+Is has not been so many years since the word *e-sports* came into the eyes of general public, and this word had been long considered to be equal to simply game playing. People who are not concerned about e-sports may find it difficult to understand why so many people are boosting e-sports now and hard to believe that many people can live by their jobs about e-sports. Yet the industrial of e-sports has been on its track since 1998 in Korea.  
+### 2. The industry chain of e-sports
+
+concentric circles
+
+```mermaid
+graph TD;
+participants
+spectators
+game-providers
+competition-organizers
+league
+club
+media
+TV
+live-streaming
+sponsers
+investers
+regulator
+
+```
+<!-- ### The industrial circle -->
+
+### 3. The difference between countries about esports
+
+#### 3-1. The US, Canada and Europe
+
+#### 3-2. Korea
+
+#### 3-3. China
+
+#### 3-4. Other developing countries.
+
+### 4. Analyzation
+
+
+### 5. Conclusion
+
+---
+
+## Passage 3. Exclusive interview: Sasha “Scarlett” Hostyn
+
+William Wang, **zeweiwang@outlook.com** 
+
+<!-- 
+
+ref: https://wcs.starcraft2.com/en-us/news/21509419/Scarlett,-NoRegreT,-and-the-House-Changing-StarCraft-II-Esports/
+
+ref: https://www.pastemagazine.com/articles/2018/02/sasha-scarlett-hostyn-becomes-first-woman-to-win-a.html
+
+ref: https://www.zapmeta.com/?q=sasha+scarlett+hostyn&dzn=&asid=0028450762&where=web_zapmeta&awc=&template=&pp=&bkw=y&de=&nwc=&rkb=b&rkln=1
+
+
+ -->
+
+<!-- ref: https://www.natlawreview.com/article/esports-insights-and-trends-staying-current-emerging-developments-booming-esports-0 -->
+ 
